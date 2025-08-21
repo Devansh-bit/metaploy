aa36c9a15f41ac721e0c9100da8eabebc7f4b63a
Devansh-bit <devanshg308@gmail.com>
2025-08-21 12:46:03 +0530

    test

--- nginx/watch_reload.sh
+++ nginx/watch_reload.sh
@@ -4,24 +4,24 @@
 # Thanks to https://cyral.com/blog/how-to-auto-reload-nginx/
 
 watch_reloads() {
-	if [[ $(command -v inotifywait) != "" ]]; then
-		while true; do
-			inotifywait --exclude .swp -e create -e modify -e delete -e move /etc/nginx/sites-enabled/
+    if [[ $(command -v inotifywait) != "" ]]; then
+        while true; do
+            inotifywait --exclude .swp -e create -e modify -e delete -e move /etc/nginx/sites-enabled/ /etc/nginx/https-enabled/
 
-			echo "Detected Nginx configuration change."
+            echo "Detected Nginx configuration change."
 
-			# Test and reload the new configuration
-			if nginx -t; then
-				echo "Configuration OK. Executing: nginx -s reload"
-				nginx -s reload
-			else
-				echo "Error in configuration. Did not reload Nginx."
-			fi
-		done
-	else
-		echo "Inotifywait not found. Configuration reloads cannot be done."
-		exit 1
-	fi
+            # Test and reload the new configuration
+            if nginx -t; then
+                echo "Configuration OK. Executing: nginx -s reload"
+                nginx -s reload
+            else
+                echo "Error in configuration. Did not reload Nginx."
+            fi
+        done
+    else
+        echo "Inotifywait not found. Configuration reloads cannot be done."
+        exit 1
+    fi
 }
 
 watch_reloads >/app/nginx_auto_reload_service.log 2>&1 &
